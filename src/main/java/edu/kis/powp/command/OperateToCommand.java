9e84287aa8b2f5a829b4dc24449caa914bf46f30
Wojciech Korbel <251169@edu.p.lodz.pl>
2026-03-19 11:19:20 +0100

    Dostosowanie hierarchii polecen

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -5,23 +5,15 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class OperateToCommand implements DriverCommand {
     private final int x;
     private final int y;
-    private Job2dDriver driver;
 
     public OperateToCommand(int x, int y, Job2dDriver driver) {
         this.x = x;
         this.y = y;
-        this.driver = driver;
     }
-
-    public void setDriver(Job2dDriver d){
-        this.driver = d;
-    }
-
-
 
     @Override
-    public void execute() {
-        driver.operateTo(x, y);
+    public void execute(Job2dDriver d) {
+        d.operateTo(x, y);
     }
 
 }
--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -5,21 +5,14 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class SetPositionCommand implements DriverCommand {
     private final int x;
     private final int y;
-    private Job2dDriver driver;
-
 
     public SetPositionCommand(int x, int y, Job2dDriver driver) {
         this.x = x;
         this.y = y;
-        this.driver = driver;
-    }
-
-    public void setDriver(Job2dDriver d){
-        this.driver = d;
     }
 
     @Override
-    public void execute() {
-        driver.setPosition(x, y);
+    public void execute(Job2dDriver d) {
+        d.setPosition(x, y);
     }
 }
