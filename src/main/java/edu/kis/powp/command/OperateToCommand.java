1338ed06bca87dc04b45def3451c4529e519e284
Wojciech Korbel <251169@edu.p.lodz.pl>
2026-03-19 11:11:57 +0100

    Implementacja hierarchii klas

---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,27 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+    private final int x;
+    private final int y;
+    private Job2dDriver driver;
+
+    public OperateToCommand(int x, int y, Job2dDriver driver) {
+        this.x = x;
+        this.y = y;
+        this.driver = driver;
+    }
+
+    public void setDriver(Job2dDriver d){
+        this.driver = d;
+    }
+
+
+
+    @Override
+    public void execute() {
+        driver.operateTo(x, y);
+    }
+
+}
